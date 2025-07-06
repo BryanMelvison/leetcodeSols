c913f6066b859faba7b08377edf8260f23972f52
Bryan Melvison <bryanmel@connect.hku.hk>
2025-07-06 17:48:44 +0700

    Add comments

--- 1004.py
+++ 1004.py
@@ -2,6 +2,12 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
+        # I use a sliding window approach to find the longest subarray with at most k zeros.
+        # Time complexity, O(n) : We iterate through the list once
+        # Space complexity, O(1) : We use a few variables to keep track of the pointers and the count of zeros
+        # Performance:
+        # Runtime: faster than 78.79%
+        # Memory Usage: less than 80.63%.
         # Ill try to do it with sliding window:
         first_ptr = 0
         zero_track = 0
